3fe1876d336b10c42f9e3d90aef2949911a6133c
Abubakr <Mohammad.Abubakr@canada.ca>
2020-12-05 22:59:44 -0500

    Simplifying Day 5 P1 Solution

--- day5/board-pass-abubakr.py
+++ day5/board-pass-abubakr.py
@@ -4,10 +4,11 @@ def binary_to_seating(bin_number):
     return seat_row + seat_column
 
 
+def seating_to_binary(seating):
+    return '0b' + seating.replace('B', '1').replace('F', '0').replace('R', '1').replace('L', '0')
+
+
 if __name__ == '__main__':
     seats = [seat.strip() for seat in open("input").readlines()]
-    for i in range(1023, -1, -1):
-        seat = binary_to_seating(format(i, '#012b'))
-        if seat in seats:
-            print(f"Highest seat found is {seat} with ID {i}")
-            break
+    highest_ID = int(max(list(map(seating_to_binary, seats))), 2)
+    print(f"Highest seat found is {binary_to_seating(format(highest_ID, '#012b'))} with ID {highest_ID}")
