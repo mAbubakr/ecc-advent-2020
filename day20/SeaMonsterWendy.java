d659f7f247e027bd417f58bb69ea73df8fbeee4d
YuenW <YuenW@WPYRJ105961.ncr.int.ec.gc.ca>
2021-01-03 17:43:12 -0800

    day 20! wow that was brutal hahaha

--- day20/SeaMonsterWendy.java
+++ day20/SeaMonsterWendy.java
@@ -34,13 +34,179 @@ public class SeaMonsterWendy {
 //		 test(3079);
 //		 test(1259);
 		 
-		 List<Tile> tiles = findCorners();
+		 List<Tile> corners = findCorners();
 		 long product = 1;
-		 for (Tile t: tiles) {
+		 for (Tile t: corners) {
 			 t.printTile();
 			 product = product * t.tileNum;
 		 }
 		 System.out.println("Part 1: " + product); 
+		 
+		 //set one corner as the top left and stitch from there
+		 Tile topLeft = null;
+		 for (Tile t: corners) {
+			 if (t.findRightNeighbor() != null && t.findBottomNeighbor() != null) {
+				 topLeft = t;
+				 break;
+			 }				 
+		 }
+		 
+		 int mapDim = (int)Math.sqrt(tiles.size());
+		 Tile[][] map = new Tile[mapDim][mapDim];
+		 map[0][0] = topLeft;
+		 for (int y=0; y<mapDim-1; y++) {
+			 for (int x=0; x<mapDim-1; x++) {
+				 Tile r = map[y][x].findRightNeighbor();
+				 map[y][x+1] = r;
+				 Tile b = map[y][x].findBottomNeighbor();
+				 map[y+1][x] = b;
+			 }
+		 }
+		 //fill the final tile
+		 Tile r = map[mapDim-1][mapDim-2].findRightNeighbor();
+		 map[mapDim-1][mapDim-1] = r;
+		 
+		 //print the map tile numbers of sanity
+		 for (int y=0; y<mapDim; y++) {
+			 for (int x=0; x<mapDim; x++) {
+				 System.out.print(map[y][x].tileNum + " ");
+			 }
+			 System.out.println("");
+		 }
+		 
+		 //remove the borders of each tile		 
+		 //stitch together into one big tile
+		 int unit = map[0][0].dim - 2; //num of char to copy per tile
+		 int finalDim = mapDim * unit;
+		 char[][] finalMatrix = new char[finalDim][finalDim];
+		 for (int y=0; y<mapDim; y++) {
+			 int startY = y * unit;
+			 for (int x=0; x<mapDim; x++) {
+				 int startX = x * unit;
+				 char[][] matrix = map[y][x].matrix;
+				 for (int i=0; i<unit; i++) {
+					 for (int j=0; j<unit; j++) {
+						 finalMatrix[startY + i][startX + j] = matrix[i+1][j+1];
+					 }
+				 }
+			 }
+		 }
+		 
+		 //create a tile with the stitched map
+		 Tile whole = new Tile(0, finalMatrix);
+		 
+		 //search each orientation
+		 Tile temp = whole;
+		 for (;;) {
+			 if (findSeaMonster(temp)) break;
+			 temp = whole.rotate90();
+			 if (findSeaMonster(temp)) break;
+			 temp = whole.rotate180();
+			 if (findSeaMonster(temp)) break;
+			 temp = whole.rotate270();
+			 if (findSeaMonster(temp)) break;
+			 
+			 temp = whole.flipHorizontally();
+			 if (findSeaMonster(temp)) break;
+			 temp = temp.rotate90();
+			 if (findSeaMonster(temp)) break;
+			 temp = temp.rotate180();
+			 if (findSeaMonster(temp)) break;
+			 temp = whole.rotate270();
+			 if (findSeaMonster(temp)) break;
+
+			 temp = whole.flipVertically();
+			 if (findSeaMonster(temp)) break;
+			 temp = temp.rotate90();
+			 if (findSeaMonster(temp)) break;
+			 temp = temp.rotate180();
+			 if (findSeaMonster(temp)) break;
+			 temp = whole.rotate270();
+			 if (findSeaMonster(temp)) break;
+			 
+		 }
+
+		 temp.printTile();
+		 
+		 System.out.println("Part 2: " + countRoughWater(temp));
+		 
+		 	 
+	}
+
+	private int countRoughWater(Tile t) {
+		int count=0;
+		int dim = t.dim;
+		char[][] matrix = t.matrix;
+		for (int y = 0; y < dim; y++) {
+			for (int x = 0; x < dim; x++) {
+				if (matrix[y][x] == '#') count++;
+			}
+		}
+		return count;		
+	}
+	
+    /*
+     *                   # 
+       #    ##    ##    ###
+        #  #  #  #  #  #   
+     */
+//	 matrix[y][x+18]
+//	 matrix[y+1][x]
+//	 matrix[y+1][x+5]
+//	 matrix[y+1][x+6]				 
+//	 matrix[y+1][x+11]
+//	 matrix[y+1][x+12]				 
+//	 matrix[y+1][x+17]
+//	 matrix[y+1][x+18]				 
+//	 matrix[y+1][x+19]
+//	 matrix[y+2][x+1]
+//	 matrix[y+2][x+4]
+//	 matrix[y+2][x+7]						 
+//	 matrix[y+2][x+10]
+//	 matrix[y+2][x+13]
+//	 matrix[y+2][x+16]						 
+
+	
+	/**
+	 * Search for sea monsters and replace the char with 0 for identified monsters
+	 * 
+	 * @param t
+	 * @return true if sea monster found
+	 */
+	private boolean findSeaMonster(Tile t) {
+		boolean found = false;
+		int dim = t.dim;
+		char[][] matrix = t.matrix;
+		for (int y = 0; y < dim - 2; y++) {
+			for (int x = 0; x < dim - 19; x++) {
+				if (matrix[y][x + 18] == '#' && matrix[y + 1][x] == '#' && matrix[y + 1][x + 5] == '#'
+						&& matrix[y + 1][x + 6] == '#' && matrix[y + 1][x + 11] == '#' && matrix[y + 1][x + 12] == '#'
+						&& matrix[y + 1][x + 17] == '#' && matrix[y + 1][x + 18] == '#' && matrix[y + 1][x + 19] == '#'
+						&& matrix[y + 2][x + 1] == '#' && matrix[y + 2][x + 4] == '#' && matrix[y + 2][x + 7] == '#'
+						&& matrix[y + 2][x + 10] == '#' && matrix[y + 2][x + 13] == '#'
+						&& matrix[y + 2][x + 16] == '#') {
+					found = true;
+					matrix[y][x + 18] = '0';
+					matrix[y + 1][x] = '0';
+					matrix[y + 1][x + 5] = '0';
+					matrix[y + 1][x + 6] = '0';
+					matrix[y + 1][x + 11] = '0';
+					matrix[y + 1][x + 12] = '0';
+					matrix[y + 1][x + 17] = '0';
+					matrix[y + 1][x + 18] = '0';
+					matrix[y + 1][x + 19] = '0';
+					matrix[y + 2][x + 1] = '0';
+					matrix[y + 2][x + 4] = '0';
+					matrix[y + 2][x + 7] = '0';
+					matrix[y + 2][x + 10] = '0';
+					matrix[y + 2][x + 13] = '0';
+					matrix[y + 2][x + 16] = '0';
+
+				}
+
+			}
+		}
+		return found;
 	}
 	
 	//match by no rotation
@@ -57,20 +223,20 @@ public class SeaMonsterWendy {
 		int matches = 0;
 		for (Tile other: tiles.values()) {
 			if (t.tileNum == other.tileNum) continue; //don't count itself
-			if (isMatch(t, other)) { matches++; continue; }
-			if (isMatch(t, other.rotate90())) { matches++; continue; }
-			if (isMatch(t, other.rotate180())) { matches++; continue; }			
-			if (isMatch(t, other.rotate270())) { matches++; continue; }			
+			if (isMatch(t, other)) { matches++; t.addNeighbor(other); continue; }
+			if (isMatch(t, other.rotate90())) { matches++; t.addNeighbor(other); continue; }
+			if (isMatch(t, other.rotate180())) { matches++; t.addNeighbor(other); continue; }			
+			if (isMatch(t, other.rotate270())) { matches++; t.addNeighbor(other); continue; }			
 			Tile otherFlipped = other.flipHorizontally();
-			if (isMatch(t, otherFlipped)) { matches++; continue; }
-			if (isMatch(t, otherFlipped.rotate90())) { matches++; continue; }
-			if (isMatch(t, otherFlipped.rotate180())) { matches++; continue; }			
-			if (isMatch(t, otherFlipped.rotate270())) { matches++; continue; }			
+			if (isMatch(t, otherFlipped)) { matches++; t.addNeighbor(other); continue; }
+			if (isMatch(t, otherFlipped.rotate90())) { matches++; t.addNeighbor(other); continue; }
+			if (isMatch(t, otherFlipped.rotate180())) { matches++; t.addNeighbor(other); continue; }			
+			if (isMatch(t, otherFlipped.rotate270())) { matches++; t.addNeighbor(other); continue; }			
 			otherFlipped = other.flipVertically();
-			if (isMatch(t, otherFlipped)) { matches++; continue; }
-			if (isMatch(t, otherFlipped.rotate90())) { matches++; continue; }
-			if (isMatch(t, otherFlipped.rotate180())) { matches++; continue; }			
-			if (isMatch(t, otherFlipped.rotate270())) { matches++; continue; }					
+			if (isMatch(t, otherFlipped)) { matches++; t.addNeighbor(other); continue; }
+			if (isMatch(t, otherFlipped.rotate90())) { matches++; t.addNeighbor(other); continue; }
+			if (isMatch(t, otherFlipped.rotate180())) { matches++; t.addNeighbor(other); continue; }			
+			if (isMatch(t, otherFlipped.rotate270())) { matches++; t.addNeighbor(other); continue; }					
 		}
 		return matches;
 	}
@@ -78,24 +244,7 @@ public class SeaMonsterWendy {
 	private List<Tile> findCorners() {
 		List<Tile> corners = new ArrayList<Tile>();
 		for(Tile t: tiles.values()) {
-			//test each orientation
-			if (getMatches(t) > 2) continue;
-			if (getMatches(t.rotate90()) > 2) continue;
-			if (getMatches(t.rotate180()) > 2) continue;
-			if (getMatches(t.rotate270()) > 2) continue;
-			Tile tFlipped = t.flipHorizontally();
-			if (getMatches(tFlipped) > 2) continue;
-			if (getMatches(tFlipped.rotate90()) > 2) continue;
-			if (getMatches(tFlipped.rotate180()) > 2) continue;
-			if (getMatches(tFlipped.rotate270()) > 2) continue;
-			tFlipped = t.flipVertically();
-			if (getMatches(tFlipped) > 2) continue;
-			if (getMatches(tFlipped.rotate90()) > 2) continue;
-			if (getMatches(tFlipped.rotate180()) > 2) continue;
-			if (getMatches(tFlipped.rotate270()) > 2) continue;
-			
-			//else none of its orientation has more than 2 matches, is a corner
-			corners.add(t);			
+			if (getMatches(t) == 2)	corners.add(t);			
 		}		
 		return corners;
 	}
@@ -109,6 +258,7 @@ public class SeaMonsterWendy {
 		 		
 		Tile t = tiles.get(num);
 
+		
 		System.out.println("Original");
 		t.printTile();
 
@@ -135,8 +285,9 @@ public class SeaMonsterWendy {
 
 	private class Tile {
 		public int tileNum;
-		private int dim;
-		private char[][] matrix;
+		public int dim;
+		public char[][] matrix;
+		public List<Tile> neighbors = new ArrayList<Tile>();
 		
 		public Tile(int tileNum, List<String> input) {
 			this.tileNum = tileNum;
@@ -153,6 +304,10 @@ public class SeaMonsterWendy {
 			this.tileNum = tileNum;
 			this.dim = matrix.length;
 			this.matrix = matrix;
+		}
+		
+		public void addNeighbor(Tile t) {
+			neighbors.add(t);
 		}
 		
 		public void printTile() {
@@ -239,7 +394,65 @@ public class SeaMonsterWendy {
 		
 		public Tile rotate270() {
 			return this.rotate90().rotate90().rotate90();
-		}						
+		}		
+		
+		public Tile findRightNeighbor() {
+			for (Tile other: neighbors) {
+				if (Arrays.compare(this.getRight(), other.getLeft()) == 0) { return other; }
+				Tile otherFlipped = other.rotate90();
+				if (Arrays.compare(this.getRight(), otherFlipped.getLeft()) == 0) { otherFlipped.neighbors = other.neighbors; return otherFlipped; }
+				otherFlipped = other.rotate180();
+				if (Arrays.compare(this.getRight(), otherFlipped.getLeft()) == 0) { otherFlipped.neighbors = other.neighbors; return otherFlipped; }
+				otherFlipped = other.rotate270();
+				if (Arrays.compare(this.getRight(), otherFlipped.getLeft()) == 0) { otherFlipped.neighbors = other.neighbors; return otherFlipped; }				
+				otherFlipped = other.flipHorizontally();				
+				if (Arrays.compare(this.getRight(), otherFlipped.getLeft()) == 0) { otherFlipped.neighbors = other.neighbors; return otherFlipped; }
+				otherFlipped = otherFlipped.rotate90();
+				if (Arrays.compare(this.getRight(), otherFlipped.getLeft()) == 0) { otherFlipped.neighbors = other.neighbors; return otherFlipped; }
+				otherFlipped = otherFlipped.rotate180();
+				if (Arrays.compare(this.getRight(), otherFlipped.getLeft()) == 0) { otherFlipped.neighbors = other.neighbors; return otherFlipped; }
+				otherFlipped = otherFlipped.rotate270();
+				if (Arrays.compare(this.getRight(), otherFlipped.getLeft()) == 0) { otherFlipped.neighbors = other.neighbors; return otherFlipped; }				
+				otherFlipped = other.flipVertically();
+				if (Arrays.compare(this.getRight(), otherFlipped.getLeft()) == 0) { otherFlipped.neighbors = other.neighbors; return otherFlipped; }
+				otherFlipped = otherFlipped.rotate90();
+				if (Arrays.compare(this.getRight(), otherFlipped.getLeft()) == 0) { otherFlipped.neighbors = other.neighbors; return otherFlipped; }
+				otherFlipped = otherFlipped.rotate180();
+				if (Arrays.compare(this.getRight(), otherFlipped.getLeft()) == 0) { otherFlipped.neighbors = other.neighbors; return otherFlipped; }
+				otherFlipped = otherFlipped.rotate270();
+				if (Arrays.compare(this.getRight(), otherFlipped.getLeft()) == 0) { otherFlipped.neighbors = other.neighbors; return otherFlipped; }
+			}
+			return null;
+		}
+		
+		public Tile findBottomNeighbor() {
+			for (Tile other: neighbors) {
+				if (Arrays.compare(this.getBottom(), other.getTop()) == 0) { return other; }
+				Tile otherFlipped = other.rotate90();
+				if (Arrays.compare(this.getBottom(), otherFlipped.getTop()) == 0) { otherFlipped.neighbors = other.neighbors; return otherFlipped; }
+				otherFlipped = other.rotate180();
+				if (Arrays.compare(this.getBottom(), otherFlipped.getTop()) == 0) { otherFlipped.neighbors = other.neighbors; return otherFlipped; }
+				otherFlipped = other.rotate270();
+				if (Arrays.compare(this.getBottom(), otherFlipped.getTop()) == 0) { otherFlipped.neighbors = other.neighbors; return otherFlipped; }				
+				otherFlipped = other.flipHorizontally();				
+				if (Arrays.compare(this.getBottom(), otherFlipped.getTop()) == 0) { otherFlipped.neighbors = other.neighbors; return otherFlipped; }
+				otherFlipped = otherFlipped.rotate90();
+				if (Arrays.compare(this.getBottom(), otherFlipped.getTop()) == 0) { otherFlipped.neighbors = other.neighbors; return otherFlipped; }
+				otherFlipped = otherFlipped.rotate180();
+				if (Arrays.compare(this.getBottom(), otherFlipped.getTop()) == 0) { otherFlipped.neighbors = other.neighbors; return otherFlipped; }
+				otherFlipped = otherFlipped.rotate270();
+				if (Arrays.compare(this.getBottom(), otherFlipped.getTop()) == 0) { otherFlipped.neighbors = other.neighbors; return otherFlipped; }				
+				otherFlipped = other.flipVertically();
+				if (Arrays.compare(this.getBottom(), otherFlipped.getTop()) == 0) { otherFlipped.neighbors = other.neighbors; return otherFlipped; }
+				otherFlipped = otherFlipped.rotate90();
+				if (Arrays.compare(this.getBottom(), otherFlipped.getTop()) == 0) { otherFlipped.neighbors = other.neighbors; return otherFlipped; }
+				otherFlipped = otherFlipped.rotate180();
+				if (Arrays.compare(this.getBottom(), otherFlipped.getTop()) == 0) { otherFlipped.neighbors = other.neighbors; return otherFlipped; }
+				otherFlipped = otherFlipped.rotate270();
+				if (Arrays.compare(this.getBottom(), otherFlipped.getTop()) == 0) { otherFlipped.neighbors = other.neighbors; return otherFlipped; }
+			}
+			return null;
+		}		
 	}
 	
 	public static void main(String[] args) throws IOException {
